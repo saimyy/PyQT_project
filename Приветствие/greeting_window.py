ea09907362f70be00ac0737990f48200d49cb5f9
saimyyy <chshiev07@mail.ru>
2023-10-20 19:42:37 +0300

    version 1

---
+++ Приветствие/greeting_window.py
@@ -0,0 +1,21 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from greeting_ui import Ui_MainWindow
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi(self)
+
+    def run(self):
+        self.label.setText("OK")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
